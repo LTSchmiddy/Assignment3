9d9586c257295296d5a2407a36b0b65e404057be
LTSchmiddy <schmidalex1996@gmail.com>
2020-05-09 01:25:33 -0400

    timestamp fix

--- blueprints/api.py
+++ blueprints/api.py
@@ -69,7 +69,7 @@ def update_entry():
     r_date_due = request.form.get('date_due')
 
     try:
-        if r_date_due == "" or r_date_due == "None":
+        if r_date_due == "" or r_date_due == "None" or r_date_due == "T":
             entry.date_due = None
 
         else:
