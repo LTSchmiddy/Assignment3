abd5224b2f9cfe6ee45ac274136da480df9a01c1
LTSchmiddy <schmidalex1996@gmail.com>
2020-05-09 01:47:27 -0400

    reverse order

--- blueprints/panes.py
+++ blueprints/panes.py
@@ -13,7 +13,7 @@ panes = Blueprint(
 
 @panes.route('/item_list', methods=('GET', 'POST'))
 def item_list():
-    return render_template("item_button_list.html", items=TodoItem.query.order_by(TodoItem.date_added).all())
+    return render_template("item_button_list.html", items=TodoItem.query.order_by(TodoItem.date_added.desc()).all())
 
 
 @panes.route('/item_view', methods=('GET', 'POST'))
